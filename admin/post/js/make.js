3a7306fb376381844add7cd139e02285cc95efb6
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-04-29 12:12:38 +0200

    Fix: Changed the edit post layout. Removed paragraphs and replaced with single body string.

--- admin/post/js/make.js
+++ admin/post/js/make.js
@@ -1,23 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    const createPostFormContainer = document.querySelector('#form-section-wrapping');
+    const createPostForm = document.getElementById('create-post-form');
 
-    const newBlogPostData = {
+    createPostForm.addEventListener('submit', handleFormSubmit);
+
+    async function handleFormSubmit(event) {
+            event.preventDefault(); //prevent the user to be redirected to another page when submitting form
+        const form = event.currentTarget;
 
     }
 
-    function createNewPost() {
-        const username = sessionStorage.getItem('username');
-        fetch(`https://v2.api.noroff.dev/blog/posts/${username}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify()
-        }).then(response => response.json())
-            .then(post => {
-                populateForm(formElement, post);
-            })
-            .catch(error => console.error('Failed to fetch post for editing:', error));
-    }
+    //     const username = sessionStorage.getItem('username');
+    //     fetch(`https://v2.api.noroff.dev/blog/posts/${username}`, {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify()
+    //     }).then(response => response.json())
+    //         .then(post => {
+    //
+    //         })
+    //         .catch(error => console.error('Failed to fetch post for editing:', error));
+    // }
+
+//         function (event) {
+//         event.preventDefault(); //prevent the user to be redirected to another page when submitting form
+//
+//         const payload = new FormData(createPostForm);
+//         console.log([...payload]);
+//
+
 });
---
+++ user/js/single-blog-page.js
@@ -0,0 +1,29 @@
+
+function displaySinglePost () {
+    fetch('https://v2.api.noroff.dev/blog/posts/IngridOrnum/bdaaf12a-be56-4ad7-a4b8-5f03bf0f00d3')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            const postDataDisplay =
+                `
+                <img src="${data.data.media.url}" alt="${data.data.media.alt}">
+                <h1>${data.data.title}</h1>
+                <p>${data.data.body}</p>
+         
+                `
+            document.querySelector('.blog-post-container').insertAdjacentHTML('beforeend', postDataDisplay);
+
+        })
+        .catch(error => console.log(error));
+}
+
+displaySinglePost()
+
+
+
+
+
+
+
