5cdf5768169a0dad6ee648e7d6869c3cc08b2fbd
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-05-05 12:31:23 +0200

    Chore: finished pagination on carousel slider. Added html, css and added/modified js.

--- user/js/user-landing-page.js
+++ user/js/user-landing-page.js
@@ -1,5 +1,6 @@
 let currentSlideIndex = 0;
 let slideInterval;
+const paginationElement = document.querySelector('.pagination-element');
 
 function fetchUserBlogPostsCarousel() {
     fetch(`https://v2.api.noroff.dev/blog/posts/IngridOrnum`, {
@@ -23,6 +24,7 @@ function fetchUserBlogPostsCarousel() {
                 </li>`;
                 }).join('');  // Join the array of strings into a single string
                 initializeSlides(); // Initialize slides and start the automatic slideshow
+                initializePagination();
             }
         })
         .catch(error => console.error('Error fetching blog posts:', error));
@@ -39,7 +41,20 @@ function startAutomaticSlide() {
     }, 3000);
 }
 
+function initializePagination() {
+    const slides = document.querySelectorAll('.carousel-ul-container .post-slide');
+    const paginationHTML = [];
+    for (var i = 0; i < slides.length; i++) {
+        paginationHTML.push('<button class="slider-pagination-btn" data-index="' + i + '"></button>');
+    }
+    paginationElement.innerHTML = paginationHTML.join('');
+    updatePagination();
+}
+
 function showSlide(index) {
+
+    stopAutomaticSlide();
+
     const slides = document.querySelectorAll('.post-slide');
     if (index >= slides.length) index = 0; // wrap around to the first slide
     if (index < 0) index = slides.length -1; // wrap around to the last slide
@@ -48,7 +63,25 @@ function showSlide(index) {
     slides.forEach(slide => slide.style.display = 'none');
     slides[index].style.display = 'block';
     currentSlideIndex = index; //update current slide index
+    updatePagination();
 }
+
+paginationElement.addEventListener('click', function(event) {
+    const target = event.target;
+    if (target.classList.contains("slider-pagination-btn")) {
+        const newIndex = Number(target.getAttribute("data-index"));
+        showSlide(newIndex);
+    }
+});
+
+function updatePagination() {
+    const paginationBtns = paginationElement.querySelectorAll('.slider-pagination-btn');
+    paginationBtns.forEach((btn, index) => {
+            btn.classList.toggle('slider-pagination-btn--sel', index === currentSlideIndex);
+    });
+}
+
+// code inspiration for pagination: https://www.codeguage.com/tutorials/sliders/pagination
 
 function prevSlide() {
     stopAutomaticSlide();
