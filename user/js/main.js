f978cca284ab40926eb5bdde1afacc9eee598328
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-05-26 21:04:20 +0200

    Chore: made the burger menu functionable.

---
+++ user/js/main.js
@@ -0,0 +1,14 @@
+const dropdownContent = document.querySelector('.nav-dropdown');
+
+function openDropdown() {
+    dropdownContent.style.display = 'block';
+}
+
+function closeDropdown() {
+    dropdownContent.style.display = 'none';
+}
+
+function closeDropdownThroughLinks() {
+    dropdownContent.style.display = 'none';
+}
+
