4e771131b4e30546d2b94a779a34109973586219
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-05-29 11:43:25 +0200

    Styling: Finished styling about page. Chore: Added date and author to single blog page. Changed the formatting of the date.

--- user/js/single-blog-page.js
+++ user/js/single-blog-page.js
@@ -12,6 +12,22 @@ function displaySinglePost () {
         .then(data => {
             const postData = data.data;
 
+
+            // Your original date string
+            let dateString = postData.created;
+
+            // Create a new Date object
+            let date = new Date(dateString);
+
+            // Extract the day, month, and year
+            let day = String(date.getDate()).padStart(2, '0');
+            let month = String(date.getMonth() + 1).padStart(2, '0');
+            let year = date.getFullYear();
+
+            // Format the date as DD.MM.YYYY
+            let formattedDate = `${day}.${month}.${year}`;
+
+
             // Setting the title in head
             document.title = 'Nordic Trekking ✵ ' + postData.title;
 
@@ -20,12 +36,21 @@ function displaySinglePost () {
                 <img class="hero-img-single-post" src="${postData.media.url}" alt="${postData.media.alt}">
                 <div class="single-post-content-wrapper">
                 <div class="bg-title">
-                <h1>${postData.title}</h1>
+                    <h1>${postData.title}</h1>
                 </div>
-                    <div class="body-single-post">${postData.body}</div>
+                <div class="auth-and-date">
+                    <span>${postData.author.name}</span>
+                    <span>•</span>
+                    <span>${formattedDate}</span>
+                </div>
+                <div class="body-single-post">${postData.body}</div>
                 </div>
                 `;
             document.querySelector('.blog-post-container').insertAdjacentHTML('beforeend', postDataDisplay);
+
+            console.log(formattedDate); // Outputs: 27.05.2024
+
+
         })
         .catch(error => console.log(error));
 
