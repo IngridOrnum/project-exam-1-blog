307e96e96e855ccbdfef6f3c764c3f208f311258
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-05-30 14:56:39 +0200

    Fix: Cleaned up formatting in all js. Deleted unused filter button.

--- user/js/single-blog-page.js
+++ user/js/single-blog-page.js
@@ -10,50 +10,36 @@ function displaySinglePost () {
     fetch(`https://v2.api.noroff.dev/blog/posts/IngridOrnum/${postId}`)
         .then(response => response.json())
         .then(data => {
+
             const postData = data.data;
 
-
-            // Your original date string
             let dateString = postData.created;
-
-            // Create a new Date object
             let date = new Date(dateString);
-
-            // Extract the day, month, and year
             let day = String(date.getDate()).padStart(2, '0');
             let month = String(date.getMonth() + 1).padStart(2, '0');
             let year = date.getFullYear();
-
-            // Format the date as DD.MM.YYYY
             let formattedDate = `${day}.${month}.${year}`;
 
-
-            // Setting the title in head
             document.title = 'Nordic Trekking ✵ ' + postData.title;
 
             const postDataDisplay =
                 `
                 <img class="hero-img-single-post" src="${postData.media.url}" alt="${postData.media.alt}">
                 <div class="single-post-content-wrapper">
-                <div class="bg-title">
-                    <h1>${postData.title}</h1>
-                </div>
-                <div class="auth-and-date">
-                    <span>${postData.author.name}</span>
-                    <span>•</span>
-                    <span>${formattedDate}</span>
-                </div>
-                <div class="body-single-post">${postData.body}</div>
+                    <div class="bg-title">
+                        <h1>${postData.title}</h1>
+                    </div>
+                    <div class="auth-and-date">
+                        <span>${postData.author.name}</span>
+                        <span>•</span>
+                        <span>${formattedDate}</span>
+                    </div>
+                    <div class="body-single-post">${postData.body}</div>
                 </div>
                 `;
             document.querySelector('.blog-post-container').insertAdjacentHTML('beforeend', postDataDisplay);
-
-            console.log(formattedDate); // Outputs: 27.05.2024
-
-
         })
         .catch(error => console.log(error));
-
 }
 
 document.addEventListener('DOMContentLoaded', displaySinglePost);
